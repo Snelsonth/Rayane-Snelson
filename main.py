388b8e2ce19c4611cb320ad02a2eec3d4c83d37d
Your Name <you@example.com>
2024-01-27 19:48:03 +0100

    commit initi

---
+++ main.py
@@ -0,0 +1,25 @@
+# Program to check if a number is prime or not
+num = 67
+
+# To take input from the user
+# num = int(input("Enter a number: "))
+
+# Define a flag variable
+flag = False
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # Check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # If factor is found, set flag to True
+            flag = True
+            # Break out of loop
+            break
+
+    # Check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
